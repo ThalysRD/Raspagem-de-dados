a92155bf3a79a103addecfe35f8a4e6b5aca30e4
Thalys Rodrigues <thalysrd@pop-os.localdomain>
2023-01-18 15:51:30 -0300

    Feat: Implementa a função search_by_title, que retorna o title e a url da nótica

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,12 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = []
+    for new in search_news({"title": {"$regex": title, "$options": "i"}}):
+        news.append((new['title'], new['url']))
+    return news
 
 
 # Requisito 7
